00c2b336fc5c2380efb91cdbba567314145cb1d2
Mateus Leao <mateus.san.tos@hotmail.com>
2026-02-24 23:10:14 -0300

    feat: ajuste atividades e imagens home

--- src/app/pages/atividades/atividades.component.ts
+++ src/app/pages/atividades/atividades.component.ts
@@ -10,13 +10,16 @@ import { SectionCarouselComponent } from '@app/shared/components/section-carouse
 export class AtividadesComponent {
   title = 'ATIVIDADES';
   titleColor = '#D8CD16';
-  description = `Realizamos um conjunto amplo e contínuo de atividades voltadas ao fortalecimento das pessoas e comunidades com as quais trabalhamos. Entre elas constam oficinas práticas de geração de renda, capacitação profissional, atividades educativas para crianças e jovens, ações de saúde preventiva e prevenção, rodas de diálogo e formação de lideranças locais. As ações são planejadas em parceria com moradores, voluntários e instituições, com foco na participação ativa da comunidade, na promoção da autonomia, na transferência de conhecimento e na criação de redes de apoio duradouras que permitam acompanhar resultados ao longo do tempo.`;
+  description = `Para cumprir o tripé que norteia nosso trabalho, oferecemos aulas de jiu-jitsu, capoeira, futebol, balé, reforço escolar, informática básica e avançada e arteterapia. Também oferecemos apoio psicológico, com terapia individual e encontros em grupo com temas determinados. Além disso, promovemos rodas de leitura na biblioteca e atividades de recreação criativa com o suporte da nossa brinquedoteca.`;
 
   images = [
-    { src: '/carrossel-quem-somos/image1.svg' },
-    { src: '/carrossel-quem-somos/image2.svg' },
-    { src: '/carrossel-quem-somos/image3.svg' },
-    { src: '/carrossel-quem-somos/image4.svg' },
-    { src: '/carrossel-quem-somos/image5.svg' }
+    { src: '/atividades/image1.jpeg' },
+    { src: '/atividades/image2.jpeg' },
+    { src: '/atividades/image3.jpeg' },
+    { src: '/atividades/image4.jpeg' },
+    { src: '/atividades/image5.jpeg' },
+    { src: '/atividades/image6.jpeg' },
+    { src: '/atividades/image7.jpeg' },
+    { src: '/atividades/image8.jpeg' }
   ];
 }
