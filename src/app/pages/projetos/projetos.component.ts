2edfe78a86184aed1a6dbf3096abfa664133b22a
Mateus Leao <mateus.san.tos@hotmail.com>
2026-03-08 23:24:42 -0300

    feat: novas paginas

---
+++ src/app/pages/projetos/projetos.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-projetos',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  templateUrl: './projetos.component.html',
+  styleUrls: ['./projetos.component.css']
+})
+export class ProjetosComponent {
+  projects2025 = [
+    { title: 'Projeto Alpha', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Projeto Beta', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Projeto Gamma', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+  ];
+
+  projects2024 = [
+    { title: 'Projeto Delta', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Projeto Epsilon', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Projeto Zeta', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+  ];
+}
